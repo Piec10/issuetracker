6313fa181e734d230bbda6ad741d18fb4c6befc2
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-11-12 17:25:28 +0100

    add basic spring security

--- src/main/java/com/piec10/issuetracker/config/SecurityConfig.java
+++ src/main/java/com/piec10/issuetracker/config/SecurityConfig.java
@@ -1,37 +1,32 @@
 package com.piec10.issuetracker.config;
 
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.provisioning.JdbcUserDetailsManager;
-//import org.springframework.security.provisioning.UserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public DataSource dataSource(){
-//        return DataSourceBuilder.create().build();
-//    }
-//
-////    @Bean
-//    public UserDetailsManager users(DataSource dataSource){
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        http.authorizeHttpRequests()
-//                .antMatchers("/").permitAll();
-//
-//        return http.build();
-//    }
+    @Bean
+    public UserDetailsManager users(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .antMatchers("/**").hasRole("ADMIN")
+                .and()
+                .formLogin().permitAll();
+
+        return http.build();
+    }
 }
