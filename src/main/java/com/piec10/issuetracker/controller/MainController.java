a54ce7eb778da1fff324e4ca2a8af37677ae58c3
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-11-15 19:53:16 +0100

    add about and contact pages

--- src/main/java/com/piec10/issuetracker/controller/MainController.java
+++ src/main/java/com/piec10/issuetracker/controller/MainController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DemoController {
+public class MainController {
 
     @GetMapping("/home")
     public String getHome(){
         return "home";
+    }
+
+    @GetMapping("/contact")
+    public String getContact(){
+        return "contact";
+    }
+
+    @GetMapping("/about")
+    public String getAbout(){
+        return "about";
     }
 }
