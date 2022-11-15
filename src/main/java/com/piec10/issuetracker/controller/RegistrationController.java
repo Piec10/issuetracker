67c5e61e8042bf4b8eedd46e7e7e04717c65a0a6
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-11-15 17:40:16 +0100

    add sign up button to home page

--- src/main/java/com/piec10/issuetracker/controller/RegistrationController.java
+++ src/main/java/com/piec10/issuetracker/controller/RegistrationController.java
@@ -2,7 +2,10 @@ package com.piec10.issuetracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +16,11 @@ public class RegistrationController {
     public String showRegistrationPage(Model theModel) {
 
         return "registration-form";
+    }
+
+    @PostMapping("/processRegistrationForm")
+    public String processRegistrationForm(Model theModel) {
+
+        return "home";
     }
 }
