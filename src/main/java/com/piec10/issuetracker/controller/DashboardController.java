c65a5f8f9767eb754a0b5a3012ac1530a8bddad2
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-11-19 18:45:41 +0100

    add fixed table to admin panel page

--- src/main/java/com/piec10/issuetracker/controller/DashboardController.java
+++ src/main/java/com/piec10/issuetracker/controller/DashboardController.java
@@ -1,12 +1,21 @@
 package com.piec10.issuetracker.controller;
 
+import com.piec10.issuetracker.entity.User;
+import com.piec10.issuetracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/")
     public String getDashboard(){
@@ -14,7 +23,12 @@ public class DashboardController {
     }
 
     @GetMapping("/adminPanel")
-    public String getAdminPanel(){
+    public String getAdminPanel(Model model){
+
+        List<User> users = userService.findAll();
+
+        model.addAttribute("users",users);
+
         return "dashboard/admin-panel";
     }
 
