fb92103dc544b7d906525a9cdb204482199b22bf
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-12-13 22:33:50 +0100

    add project entity class

---
+++ src/main/java/com/piec10/issuetracker/entity/Project.java
@@ -0,0 +1,98 @@
+package com.piec10.issuetracker.entity;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @Column(name = "created_by")
+    private User createdBy;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    private Collection<Issue> issues;
+
+    private Collection<User> guestUsers;
+
+    private Collection<User> collaborators;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Collection<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(Collection<Issue> issues) {
+        this.issues = issues;
+    }
+
+    public Collection<User> getGuestUsers() {
+        return guestUsers;
+    }
+
+    public void setGuestUsers(Collection<User> guestUsers) {
+        this.guestUsers = guestUsers;
+    }
+
+    public Collection<User> getCollaborators() {
+        return collaborators;
+    }
+
+    public void setCollaborators(Collection<User> collaborators) {
+        this.collaborators = collaborators;
+    }
+}
