b83de57f53ed7fe72fa246692a0df94430fbe789
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-12-17 19:22:35 +0100

    add user projects table on projects page

--- src/main/java/com/piec10/issuetracker/controller/ProjectController.java
+++ src/main/java/com/piec10/issuetracker/controller/ProjectController.java
@@ -1,15 +1,28 @@
 package com.piec10.issuetracker.controller;
 
+import com.piec10.issuetracker.entity.User;
+import com.piec10.issuetracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/dashboard")
 public class ProjectController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/projects")
-    public String getProjects() {
+    public String getProjects(Principal principal, Model model) {
+
+        User currentUser =  userService.findByUsername(principal.getName());
+
+        model.addAttribute("user", currentUser);
 
         return "dashboard/projects";
     }
