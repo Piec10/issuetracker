25d03cc21e389bfc86bea338e9f545d77fe03ed2
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-12-29 00:38:26 +0100

    add test page for new mobile view design, add collapsible project details

--- src/main/java/com/piec10/issuetracker/controller/MainController.java
+++ src/main/java/com/piec10/issuetracker/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.piec10.issuetracker.controller;
 
 import com.piec10.issuetracker.dao.RoleRepository;
+import com.piec10.issuetracker.entity.Project;
 import com.piec10.issuetracker.entity.Role;
 import com.piec10.issuetracker.entity.User;
 import com.piec10.issuetracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -30,7 +35,43 @@ public class MainController {
     }
 
     @GetMapping("test")
-    public String getTest(){
+    public String getTest(Model model){
+
+        User testUser = new User();
+
+        testUser.setUsername("test user");
+
+
+        Collection<Project> projects = new ArrayList<>();
+
+        Project project = new Project();
+
+        project.setId(1);
+        project.setTitle("very long test project title ");
+        project.setDescription("description description description description description description description description description description description description");
+        project.setCreatedBy(testUser);
+        project.setGuestUsers(Arrays.asList(testUser));
+        project.setCollaborators(Arrays.asList(testUser));
+        project.setIssues(new ArrayList<>());
+
+        Project project2 = new Project();
+
+        project2.setId(2);
+        project2.setTitle("very long test project 2 title ");
+        project2.setDescription("description description description description description description description description description description description description");
+        project2.setCreatedBy(testUser);
+        project2.setGuestUsers(Arrays.asList(testUser));
+        project2.setCollaborators(Arrays.asList(testUser));
+        project2.setIssues(new ArrayList<>());
+
+        projects.add(project);
+        projects.add(project2);
+
+        testUser.setGuestProjects(projects);
+        testUser.setCollaborationProjects(projects);
+
+        model.addAttribute("projects", projects);
+        model.addAttribute("user", testUser);
         return "test";
     }
 
