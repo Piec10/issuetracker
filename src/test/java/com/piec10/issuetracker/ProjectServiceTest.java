72493a791b8c5c9b510d0841650b8412328e9bc7
Mateusz Piecka <mateusz.piecka@gmail.com>
2022-12-30 01:15:00 +0100

    add tests for update project service

--- src/test/java/com/piec10/issuetracker/ProjectServiceTest.java
+++ src/test/java/com/piec10/issuetracker/ProjectServiceTest.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -34,6 +36,13 @@ public class ProjectServiceTest {
 
     private User createdBy;
 
+    private Project project;
+
+    private Date createdAt;
+
+    private HashSet<User> guests;
+    private HashSet<User> collaborators;
+
     @BeforeEach
     public void beforeEach() {
 
@@ -45,6 +54,22 @@ public class ProjectServiceTest {
         createdBy = new User();
         createdBy.setUsername("user");
 
+        createdAt = new Date(new Date().getTime() - 1000);
+
+        project = new Project();
+        project.setTitle("Old title");
+        project.setDescription("Old description");
+        project.setCreatedBy(createdBy);
+        project.setCreatedAt(createdAt);
+
+        guests = new HashSet<>();
+        collaborators = new HashSet<>();
+
+        guests.add(createdBy);
+        collaborators.add(createdBy);
+
+        project.setGuestUsers(guests);
+        project.setCollaborators(collaborators);
     }
 
     @Test
@@ -102,4 +127,57 @@ public class ProjectServiceTest {
         assertEquals(1, capturedProject.getValue().getCollaborators().size(), "Should be only one collaborator - creator");
     }
 
+    @Test
+    public void updateValidProjectServiceNoGuestsAndCollaborators() {
+
+        int id = 7;
+
+        formProject.setId(id);
+        project.setId(id);
+
+        when(projectRepository.findById(id)).thenReturn(Optional.of(project));
+
+        projectService.updateProject(formProject);
+
+        verify(projectRepository).save(capturedProject.capture());
+
+        assertEquals(formProject.getId(), capturedProject.getValue().getId(), "Project id should match");
+        assertEquals(formProject.getTitle(), capturedProject.getValue().getTitle(), "Project title should be updated");
+        assertEquals(formProject.getDescription(), capturedProject.getValue().getDescription(), "Project description should be updated");
+
+        assertEquals(createdBy.getUsername(), capturedProject.getValue().getCreatedBy().getUsername(), "Project creator name should not change");
+
+        assertEquals(createdAt, capturedProject.getValue().getCreatedAt(), "Project creation date should not change");
+
+        assertEquals(1, capturedProject.getValue().getGuestUsers().size(), "Should be only one guest user - creator");
+        assertEquals(createdBy.getUsername(), capturedProject.getValue().getGuestUsers().stream().toList().get(0).getUsername(), "Should be only one guest user - creator");
+        assertEquals(1, capturedProject.getValue().getCollaborators().size(), "Should be only one collaborator - creator");
+        assertEquals(createdBy.getUsername(), capturedProject.getValue().getCollaborators().stream().toList().get(0).getUsername(), "Should be only one collaborator - creator");
+    }
+
+    @Test
+    public void updateValidProjectServiceWithGuestsAndCollaborators() {
+
+        int id = 5;
+
+        formProject.setId(id);
+        project.setId(id);
+
+        User user2 = new User();
+        user2.setUsername("user2");
+
+        formProject.getGuestUsers().add(user2);
+        formProject.getCollaborators().add(user2);
+
+        when(projectRepository.findById(id)).thenReturn(Optional.of(project));
+
+        projectService.updateProject(formProject);
+
+        verify(projectRepository).save(capturedProject.capture());
+
+        assertEquals(2, capturedProject.getValue().getGuestUsers().size(), "Should be two guest users");
+        assertEquals(2, capturedProject.getValue().getCollaborators().size(), "Should be two collaborators");
+        assertTrue(capturedProject.getValue().getGuestUsers().contains(user2), "user2 should be guest");
+        assertTrue(capturedProject.getValue().getCollaborators().contains(user2), "user2 should be collaborator");
+    }
 }
