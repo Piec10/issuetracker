a33e041896b42e582fcb2383c77017d35bbfaafa
Mateusz Piecka <mateusz.piecka@gmail.com>
2023-01-17 11:08:05 +0100

    create IssueType entity

---
+++ src/main/java/com/piec10/issuetracker/entity/IssueType.java
@@ -0,0 +1,47 @@
+package com.piec10.issuetracker.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "issue_types")
+public class IssueType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public IssueType() {
+    }
+
+    public IssueType(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "IssueType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
