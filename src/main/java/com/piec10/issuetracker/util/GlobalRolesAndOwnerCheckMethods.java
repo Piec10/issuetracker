44a32c6c9b925358e612dfd9103d5804d7b876a3
Mateusz Piecka <mateusz.piecka@gmail.com>
2023-01-28 20:39:11 +0100

    refactor GlobalRolesAndCheckMethods

--- src/main/java/com/piec10/issuetracker/util/GlobalRolesAndOwnerCheckMethods.java
+++ src/main/java/com/piec10/issuetracker/util/GlobalRolesAndOwnerCheckMethods.java
@@ -1,12 +1,23 @@
 package com.piec10.issuetracker.util;
 
-import com.piec10.issuetracker.entity.Project;
-import com.piec10.issuetracker.entity.User;
+import com.piec10.issuetracker.entity.HasCreator;
 
 import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
 
-public class GlobalRolesAndOwnerCheckMethods {
+public abstract class GlobalRolesAndOwnerCheckMethods {
+
+    public static boolean isAdminOrOwner(HasCreator object, HttpServletRequest request) {
+        return isAdmin(request) || isOwner(object, request);
+    }
+
+    public static boolean isAdmin(HttpServletRequest request) {
+        return request.isUserInRole("ROLE_ADMIN");
+    }
+
+    public static boolean isOwner(HasCreator object, HttpServletRequest request) {
+        return ((object != null && object.getCreatedBy() != null) ?
+                (object.getCreatedBy().getUsername().equals(request.getUserPrincipal().getName())) : false);
+    }
 
     public static boolean isGuest(HttpServletRequest request) {
         return request.isUserInRole("ROLE_GUEST");
@@ -14,32 +25,5 @@ public class GlobalRolesAndOwnerCheckMethods {
 
     public static boolean isNotGuest(HttpServletRequest request) {
         return !request.isUserInRole("ROLE_GUEST");
-    }
-
-    public static boolean isAdmin(HttpServletRequest request) {
-        return request.isUserInRole("ROLE_ADMIN");
-    }
-
-    public static boolean isAdminOrOwner(User owner, HttpServletRequest request) {
-        return isAdmin(request) || isOwner(owner, request.getUserPrincipal());
-    }
-
-    public static boolean isProjectOwner(User owner, Principal principal) {
-        return isOwner(owner, principal);
-    }
-
-    public static boolean isOwner(User owner, Principal principal) {
-        return (owner != null ? (owner.getUsername().equals(principal.getName())) : false);
-    }
-
-    public static UserProjectRoles getUserProjectRoles(Project project, User user) {
-
-        UserProjectRoles userProjectRoles = new UserProjectRoles();
-
-        userProjectRoles.setFollower(project.getFollowers().contains(user));
-        userProjectRoles.setCollaborator(project.getCollaborators().contains(user));
-        userProjectRoles.setOwner(project.getCreatedBy().equals(user));
-
-        return userProjectRoles;
     }
 }
