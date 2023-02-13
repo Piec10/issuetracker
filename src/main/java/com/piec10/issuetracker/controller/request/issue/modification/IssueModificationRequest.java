4b7d15201986f196c248b6f0f52de313eb1d334d
Mateusz Piecka <mateusz.piecka@gmail.com>
2023-02-13 14:01:49 +0100

    refactor IssueModificationRequest

--- src/main/java/com/piec10/issuetracker/controller/request/issue/modification/IssueModificationRequest.java
+++ src/main/java/com/piec10/issuetracker/controller/request/issue/modification/IssueModificationRequest.java
@@ -1,20 +1,48 @@
 package com.piec10.issuetracker.controller.request.issue.modification;
 
-import com.piec10.issuetracker.controller.request.issue.IssueRestrictedAccessRequest;
+import com.piec10.issuetracker.controller.request.RestrictedAccessRequest;
+import com.piec10.issuetracker.entity.Issue;
 import com.piec10.issuetracker.service.IssueService;
 
 import javax.servlet.http.HttpServletRequest;
 
 import static com.piec10.issuetracker.controller.request.RequestRedirections.*;
+import static com.piec10.issuetracker.util.GlobalRolesAndOwnerCheckMethods.doesNotHavePermissionToModify;
 
-public abstract class IssueModificationRequest extends IssueRestrictedAccessRequest {
+public abstract class IssueModificationRequest implements RestrictedAccessRequest {
+
+    protected IssueService issueService;
+    protected Issue issue;
+    private HttpServletRequest request;
 
     public IssueModificationRequest(IssueService issueService, int issueId, HttpServletRequest request) {
-        super(issueService, issueId, request);
+        this.issueService = issueService;
+        issue = issueService.findById(issueId);
+        this.request = request;
     }
 
     @Override
     public String redirectWhenSuccess() {
         return toCurrentProject(issue.getProject());
+    }
+
+    @Override
+    public boolean isNull() {
+        return issue == null;
+    }
+
+    @Override
+    public String redirectWhenNull() {
+        return toProjects();
+    }
+
+    @Override
+    public boolean hasNoPermission() {
+        return doesNotHavePermissionToModify(issue, request);
+    }
+
+    @Override
+    public String redirectWhenNoPermission() {
+        return toAccessDenied();
     }
 }
